e628adb8f0fd4fc75154d32ad2c5a1ecf74f4a2e
Judd Mehr <juddmehr@gmail.com>
2021-01-11 15:46:07 -0700

    add docs framework

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,13 @@ using RCDesignSuite
 makedocs(
     sitename    = "RCDesignSuite.jl",
     format      = Documenter.HTML(),
+    pages       = [
+        "Intro" => "index.md",
+        "Quick Start" => "tutorial.md",
+        "Guided Examples" => "howto.md",
+        "API Reference" => "reference.md",
+        "Theory" => "theory.md"
+    ],
     modules     = [RCDesignSuite],
     repo        = "https://github.com/BYU-Aeronautics-Club/RCDesignSuite.jl/blob/{commit}{path}#L{line}",
     authors     = "Judd Mehr <juddmehr@gmail.com>",
