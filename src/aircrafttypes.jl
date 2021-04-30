dff37322c769955c8f2f846a05c85782041d4804
Judd Mehr <juddmehr@gmail.com>
2021-04-30 13:48:57 -0600

    initiate types file for future use

---
+++ src/aircrafttypes.jl
@@ -0,0 +1,17 @@
+"""
+Conceptual aircraft type for storing conceptual design details
+"""
+struct concept
+
+
+end
+
+
+
+"""
+Preliminary aircraft type for storing prelminary design details
+"""
+struct prelim
+
+
+end
