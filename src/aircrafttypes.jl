12e0da9c4f905ac65c3fc4ce8c2850713d5102cb
atagg2 <81062807+atagg2@users.noreply.github.com>
2021-05-17 17:29:54 -0600

    Update aircrafttypes.jl

--- src/aircrafttypes.jl
+++ src/aircrafttypes.jl
@@ -1,10 +1,21 @@
-"""
-Conceptual aircraft type for storing conceptual design details
-"""
-struct concept
 
+"""
+Inputs
+wing: the area of the wing
+tail: the overall design of the tail
+propulsion: the placement and design of the propulsion system
+wieght: total weight of the aircraft
+"""
+
+struct myplane
+    wing            #Float64 describing the area of the wing in in^2
+    tail            #String describing the tail configuration
+    propulsion      #String describing placement and design for propulsion system
+    weight          #Float64 describing the overal weight of the aircraft in lbs
 
 end
+
+plane = myplane(350.0,"conventional","front propeller", 1.5)
 
 
 
