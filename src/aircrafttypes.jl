68e08a7a9a8dadc70203bf807638a7a696a517d0
atagg2 <81062807+atagg2@users.noreply.github.com>
2021-05-19 16:40:14 -0600

    Update aircrafttypes.jl

--- src/aircrafttypes.jl
+++ src/aircrafttypes.jl
@@ -1,22 +1,20 @@
 
 """
-Inputs
-wing: the area of the wing
-tail: the overall design of the tail
-propulsion: the placement and design of the propulsion system
-wieght: total weight of the aircraft
-"""
 
+Inputs
+
+* `wing::Float64`: describes the area of the wing in m^2
+* `tail::String`: describes the tail configuration
+* `propulsion::String`: describes placement and design for propulsion system
+* `weight::Float64`: describes the overall weight of the aircraft in kg
+"""
 struct concept
-    wing            #Float64 describing the area of the wing in in^2
-    tail            #String describing the tail configuration
-    propulsion      #String describing placement and design for propulsion system
-    weight          #Float64 describing the overal weight of the aircraft in lbs
+    wing::Float64           #describes the area of the wing in m^2
+    tail::String            #describes the tail configuration
+    propulsion::String      #describes placement and design for propulsion system
+    weight::Float64         #describes the overall weight of the aircraft in kg
 
 end
-
-aircraft = concept(350.0,"conventional","front propeller", 1.5)
-
 
 
 """
