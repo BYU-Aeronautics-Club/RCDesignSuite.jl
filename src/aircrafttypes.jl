4e2a291281cf573ab7441ad6571417141e0ea05c
atagg2 <andrew.tagg57@gmail.com>
2021-06-08 21:42:46 -0600

    creation of prelim struct

--- src/aircrafttypes.jl
+++ src/aircrafttypes.jl
@@ -12,6 +12,17 @@ end
 Preliminary aircraft type for storing prelminary design details
 """
 struct prelim
-
+    struct prelim
+        span
+        thickness
+        chord
+        twist angle
+        dihedral
+        angle of attack
+        sideslip angle
+        bank angle
+        ns                  #number of panels along span, for use with vortex lattice
+        nc                  #number of panels along chord
+    end
 
 end
